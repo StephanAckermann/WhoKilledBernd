92a42227b86ab0c0f88bc1cb1756c241d629cf7b
StephanAckermann <stephan.ackermann@bluemail.ch>
2024-04-13 14:00:25 +0200

    Mund bis Speiseröhre

--- src/scenes/Test2.js
+++ src/scenes/Test2.js
@@ -3,8 +3,96 @@ import { k } from "../game.js"
 import "./test1.js"
 
 k.scene("Test2", async () => {
-  const player = k.get("Bernd")[0]
-  player.destroy()
-  const bla = k.get("BGtest")[0]
-  bla.destroy()
+  k.loadSprite("Bernd", "sprites/BernddasBrot.png")
+  k.loadSprite("Mund", "sprites/MundOffen.png")
+  k.loadSprite("Mund2", "sprites/MundZu.png")
+  k.loadSprite("Croissant", "sprites/Croissant.png")
+  k.loadSprite("Toast", "sprites/toast.png")
+
+  const SPEED = 320
+
+  const Question = k.add([
+    k.text(
+      "Die Verdauung beginnt mit der mechanischen Zerkleinerung" +
+        "\n" +
+        "der Nahrung durch die Zähne im Mund.",
+    ),
+    k.pos(50, 20),
+  ])
+
+  const Mitte = k.add([
+    k.sprite("Bernd"),
+    k.pos(190, 300),
+    k.area(),
+    k.anchor("center"),
+    k.body(),
+    "Target",
+  ])
+
+  const Mund = k.add([
+    k.sprite("Mund"),
+    k.pos(190, 300),
+    k.area(),
+    k.anchor("center"),
+  ])
+
+  const BerndMove = k.add([
+    k.sprite("Bernd"),
+    k.pos(550, 200),
+    k.area(),
+    k.body(),
+  ])
+
+  const Bread = k.add([k.sprite("Toast"), k.pos(400, 250), k.area(), k.body()])
+  const Bread2 = k.add([
+    k.sprite("Croissant"),
+    k.pos(700, 300),
+    k.area(),
+    k.body(),
+  ])
+
+  k.onKeyDown("left", () => {
+    // .move() is provided by pos() component, move by pixels per second
+    BerndMove.move(-SPEED, 0)
+  })
+
+  k.onKeyDown("right", () => {
+    // .move() is provided by pos() component, move by pixels per second
+    BerndMove.move(SPEED, 0)
+  })
+
+  k.onKeyDown("up", () => {
+    // .move() is provided by pos() component, move by pixels per second
+    BerndMove.move(0, -SPEED)
+  })
+
+  k.onKeyDown("down", () => {
+    // .move() is provided by pos() component, move by pixels per second
+    BerndMove.move(0, SPEED)
+  })
+
+  BerndMove.onCollide("Target", () => {
+    k.destroy(BerndMove)
+    k.destroy(Mund)
+    const Mund2 = k.add([
+      k.sprite("Mund2"),
+      k.pos(190, 300),
+      k.area(),
+      k.anchor("center"),
+    ])
+    Mund2.onClick(() => {
+      k.go("test4")
+    })
+  })
+
+  Bread.onCollide("Target", () => {
+    k.destroy(Bread)
+  })
+  Bread2.onCollide("Target", () => {
+    k.destroy(Bread2)
+  })
+
+  BerndMove.onHover(() => {
+    k.debug.log("Probier die Pfeiltasten ;)")
+  })
 })
---
+++ src/scenes/test4.js
@@ -0,0 +1,77 @@
+import { k } from "../game.js"
+
+import "./test1.js"
+import "./Test2.js"
+
+k.scene("test4", async () => {
+  k.loadSprite("Correct", "sprites/Amylasen_funktion.png")
+  k.loadSprite("Enzyme", "sprites/Scissor.png")
+  k.loadSprite("Zucker", "sprites/Zucker.jpg")
+  k.loadSprite("Bernd", "sprites/BernddasBrot.png")
+
+  const Question = k.add([
+    k.text(
+      "Welche Enzyme sind im Mund für die Verdauung von " +
+        "\n" +
+        "Stärke (pflanzliche Kohlenhydrate) verantwortlich?",
+    ),
+    k.pos(50, 20),
+  ])
+
+  const Amylase = k.add([
+    k.sprite("Enzyme"),
+    k.pos(150, 300),
+    k.area(),
+    k.anchor("center"),
+  ])
+  Amylase.color = k.rgb(255, 0, 0)
+  const AmylaseDesc = k.add([k.text("Amylase"), k.pos(80, 450)])
+
+  const Pepsin = k.add([
+    k.sprite("Enzyme"),
+    k.pos(450, 300),
+    k.area(),
+    k.anchor("center"),
+  ])
+  Pepsin.color = k.rgb(0, 255, 0)
+  const PepsinDesc = k.add([k.text("Pepsin"), k.pos(400, 450)])
+
+  const Lipase = k.add([
+    k.sprite("Enzyme"),
+    k.pos(750, 300),
+    k.area(),
+    k.anchor("center"),
+  ])
+  Lipase.color = k.rgb(0, 0, 255)
+  const LipaseDesc = k.add([k.text("Lipase"), k.pos(700, 450)])
+
+  const FunFact = k.add([
+    k.sprite("Bernd"),
+    k.pos(920, 30),
+    k.area(),
+    k.anchor("center"),
+  ])
+
+  FunFact.onHover(() => {
+    k.debug.log(
+      "Fun Fact: Täglich produzieren unsere Speicheldrüsen 1 bis 1,5 l Speichel.",
+    )
+  })
+
+  Amylase.onClick(() => {
+    k.add([k.sprite("Correct")])
+    k.wait(8, () => {
+      k.add([k.sprite("Zucker")])
+    })
+    k.wait(16, () => {
+      k.go("Test2")
+    })
+  })
+
+  Pepsin.onClick(() => {
+    k.add([k.text("Wrong"), k.pos(400, 300)])
+  })
+  Lipase.onClick(() => {
+    k.add([k.text("Wrong"), k.pos(700, 300)])
+  })
+})
